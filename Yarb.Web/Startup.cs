dabd27c74a31ee1091962234faba3c136c559825
Adam Rogers <arogers0311@gmail.com>
2014-08-10 14:59:22 +1000

    added Yarb.Web MVC project

---
+++ Yarb.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Yarb.Web.Startup))]
+namespace Yarb.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
