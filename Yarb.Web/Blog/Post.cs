ce83059aa28d3b7bb7fc0df39e220c612635b151
Adam Rogers <arogers0311@gmail.com>
2014-08-12 08:49:30 +1000

    added Post.cs

---
+++ Yarb.Web/Blog/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Yarb.Web.Blog
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset Modified { get; set; }
+        public bool AllowComments { get; set; }
+        public List<Comment> Comments { get; set; }
+        public string Content { get; set; }
+
+    }
+
+    public class Comment
+    {
+        public string Id { get; set; }
+        public DateTimeOffset Created { get; set; }
+    }
+}
