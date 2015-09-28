c5fcff891b8dae034d77009757d7596a6c58d616
Adam Rogers <arogers0311@gmail.com>
2015-09-29 08:06:16 +1000

    replaced web project with aspnet 5 empty web project

--- Yarb.Web/Startup.cs
+++ Yarb.Web/Startup.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
 
-[assembly: OwinStartupAttribute(typeof(Yarb.Web.Startup))]
 namespace Yarb.Web
 {
-    public partial class Startup
+    public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
-            ConfigureAuth(app);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
