ce5e1fcab3c7cbe34fd18aeac36ec9a5e282063b
Adam Rogers <arogers0311@gmail.com>
2015-09-29 13:29:49 +1000

    add mvc

--- src/Yarb.Web/Startup.cs
+++ src/Yarb.Web/Startup.cs
@@ -13,10 +13,12 @@ namespace Yarb.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
